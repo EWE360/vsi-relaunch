f27690ce548e8bc7291149404928d6751cc9d0bf
EWG <edwin.wagner@ehrenberg.de>
2021-07-30 14:59:37 +0200

    Added drop down for Regelwerke and News

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 const text = document.querySelector(".nachhaltigkeit__wrapper-right p span");
 const readMore = document.querySelector(".nachhaltigkeit__read-more");
+const rwLink = document.querySelector(".navigation__bottom li:nth-child(4)");
+const newsLink = document.querySelector(".navigation__bottom li:nth-child(5)");
+const rwContent = document.querySelector(".teaser__regelwerke-menu");
+const newsContent = document.querySelector(".teaser__news-menu");
+const menuNotRegelwerke = document.querySelectorAll(
+  ".navigation__bottom li:not(:nth-child(4))"
+);
+const menuNotNews = document.querySelectorAll(
+  ".navigation__bottom li:not(:nth-child(5))"
+);
 
 readMore.addEventListener("click", toggleView);
+rwLink.addEventListener("mouseover", showRegelwerke);
+newsLink.addEventListener("mouseover", showNews);
+
+menuNotRegelwerke.forEach((item) => {
+  item.removeEventListener("mouseover", hideMenu);
+  item.addEventListener("mouseover", hideMenu);
+});
+
+menuNotNews.forEach((item) => {
+  item.removeEventListener("mouseover", hideMenu2);
+  item.addEventListener("mouseover", hideMenu2);
+});
 
 function toggleView() {
   if (text.classList.contains("hide")) {
@@ -13,4 +35,25 @@ function toggleView() {
     readMore.innerHTML =
       '<img src="img/arrow-yellow-right.svg" alt="" />' + "weiterlesen";
   }
+}
+
+function showRegelwerke() {
+  rwContent.classList.remove("hide");
+}
+
+function showNews() {
+  newsContent.classList.remove("hide");
+}
+
+document.onclick = function hideRegelwerke() {
+  rwContent.classList.add("hide");
+  newsContent.classList.add("hide");
+};
+
+function hideMenu() {
+  rwContent.classList.add("hide");
+}
+
+function hideMenu2() {
+  newsContent.classList.add("hide");
 }
